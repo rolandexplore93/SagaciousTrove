9ad1e9fef7564fc19e4a36ee16c6a857318d2ca7
rolandexplore93 <roland2rule@gmail.com>
2024-05-21 08:25:46 +0100

    SagaciousTrove project setup

--- SagaciousTrove/Models/Category.cs
+++ SagaciousTrove/Models/Category.cs
@@ -1,6 +1,14 @@
-﻿namespace SagaciousTrove.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SagaciousTrove.Models
 {
     public class Category
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
 }
