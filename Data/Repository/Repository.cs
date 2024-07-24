a9162876f7ca79b4c503616ae404094fcf85adad
Orobola Roland (Mac) <roland2rule@gmail.com>
2024-07-24 10:20:59 +0100

    Update IRepository and Repository class to accept optional properties

--- Data/Repository/Repository.cs
+++ Data/Repository/Repository.cs
@@ -14,6 +14,7 @@ namespace Data.Repository
         public Repository(ApplicationDbContext db)
         {
             _db = db; // database implementation
+            //_db.Products.Include(u => u.Category).Include(u => u.CoverType);
             // From the _db above, assign it to dbSet on the generic class T. This will set the DbSet to the particular instance of the class that is calling the repository
             this.dbSet = _db.Set<T>();
         }
@@ -23,17 +24,31 @@ namespace Data.Repository
             dbSet.Add(entity);
         }
 
-        public IEnumerable<T> GetAll()
+        // includeProperties - "Category,CoverType" i.e they are separated by a comma
+        public IEnumerable<T> GetAll(string? includeProperties = null)
         {
             // This should return IEnumerable<T> but we may want to query the data first before returnimg IEnumerable<T>. So, use IQuerable
             IQueryable<T> query = dbSet;
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
             return query.ToList();
         }
 
-        public T GetFirstOrDefault(Expression<Func<T, bool>> filter)
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
             query = query.Where(filter);
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
             return query.FirstOrDefault();
         }
 
