16717b95c4f25baa44e227ddfbb89bc9c15d3990
Orobola Roland (Mac) <roland2rule@gmail.com>
2024-07-25 13:27:14 +0100

    Implement api to delete product by id

--- SagaciousTrove/Areas/Admin/Controllers/ProductController.cs
+++ SagaciousTrove/Areas/Admin/Controllers/ProductController.cs
@@ -123,46 +123,36 @@ namespace SagaciousTrove.CoverTypeController
 
         }
 
-        [HttpGet]
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-
-            var CoverTypeFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
-
-            if (CoverTypeFromDbFirst == null)
-            {
-                NotFound();
-            }
-
-            return View(CoverTypeFromDbFirst);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeletePOST(int? id)
-        {
-            var obj = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
-            if (obj == null)
-            {
-                return BadRequest();
-            }
-
-            _unitOfWork.CoverType.Remove(obj);
-            _unitOfWork.Save();
-            TempData["Success"] = "CoverType deleted successfully";
-            return RedirectToAction("Index");
-        }
-
         #region API CALLS
         [HttpGet]
         public IActionResult GetAll()
         {
             var productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
             return Json(new { data = productList });
+        }
+
+        // POST
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var obj = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return Json(new { success = false, message = "error while deleting" });
+            }
+
+            var imageUrl = obj?.ImageUrl;
+            if (!string.IsNullOrEmpty(imageUrl))
+            {
+                var oldImagePath = Path.Combine(_hostEnvironment.WebRootPath, imageUrl.TrimStart(Path.DirectorySeparatorChar));
+                if (System.IO.File.Exists(oldImagePath))
+                {
+                    System.IO.File.Delete(oldImagePath);
+                }
+            }
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
         }
         #endregion
     }
