fe9dae0c19051dce38c964fbee1777f497e5e73a
Orobola Roland (Mac) <roland2rule@gmail.com>
2024-08-02 10:11:23 +0100

    Enable Email notification for account creation and order placed

--- SagaciousTrove/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SagaciousTrove/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -202,8 +202,8 @@ namespace SagaciousTrove.Areas.Identity.Pages.Account
                         values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
                         protocol: Request.Scheme);
 
-                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    await _emailSender.SendEmailAsync(Input.Email, "Account Creation Successful - Confirm your email",
+                        $"Your account has been created. Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
